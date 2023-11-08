481eb549d07f2a1eab8d2c7a1c7011cdc0d3e0f4
Lukas Werner <lukas.m.werner@outlook.com>
2023-11-07 18:18:13 -0700

    fix the example

--- pycode.py
+++ pycode.py
@@ -1,13 +1,13 @@
-import library
+import imagediff
 
 # with open("a.png", "rb") as a:
 #     with open("b.png", "rb") as b:
 #         difference = library.compare(a.read(), b.read())
 #         print(difference)
 
-with open("data_chart_other.png", 'rb') as a:
+with open("data_chart.png", 'rb') as a:
     with open("data_chart_other.png", 'rb') as b:
-        difference = library.compare(a.read(), b.read())
+        difference = imagediff.compare(a.read(), b.read())
         print(difference)
 
 
